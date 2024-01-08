c1f09b3b0dfb01a5bc06d31f91c7cb49036e4b8d
Tim Blume <tbl@sevenstax-intern.de>
2024-01-08 01:31:42 +0100

    tmp

--- src/etcetera.hpp
+++ src/etcetera.hpp
@@ -9,10 +9,16 @@
 
 namespace etcetera {
 
+class Struct;
+
 class Base {
 protected:
+  std::type_info const &type_ = typeid(Base);
+
   Base *parent = nullptr;
   friend class Struct;
+
+  void set_parent(Base *parent) { this->parent = parent; }
 
 public:
   virtual std::any parse(std::iostream &stream) = 0;
@@ -21,19 +27,15 @@ public:
   virtual bool is_struct() { return false; }
   virtual bool is_array() { return false; }
 
-  // for structs and the like
-  virtual std::any get(std::string) {
-    throw std::runtime_error("Not implemented");
-  }
   // returns all the data
   virtual std::any get() = 0;
 };
 
 typedef std::pair<std::string, Base *> Field;
 
-class Int32ul : public Base {
+template <typename T> class IntegralType : public Base {
 public:
-  int32_t value = 0;
+  T value = 0;
   std::any parse(std::iostream &stream) override {
     stream.read(reinterpret_cast<char *>(&value), sizeof(value));
     return value;
@@ -43,6 +45,14 @@ public:
   }
   std::any get() override { return value; }
 };
+using Int8ul = IntegralType<int8_t>;
+using Int16ul = IntegralType<int16_t>;
+using Int32ul = IntegralType<int32_t>;
+using Int64ul = IntegralType<int64_t>;
+using UInt8ul = IntegralType<uint8_t>;
+using UInt16ul = IntegralType<uint16_t>;
+using UInt32ul = IntegralType<uint32_t>;
+using UInt64ul = IntegralType<uint64_t>;
 
 class Struct : public Base {
   std::map<std::string, Base *> fields;
@@ -52,6 +62,10 @@ public:
     (fields.emplace(std::get<0>(std::forward<Args>(args)),
                     std::get<1>(std::forward<Args>(args))),
      ...);
+
+    for (auto &[key, field] : fields) {
+      field->set_parent(this);
+    }
   }
 
   std::any parse(std::iostream &stream) override {
@@ -72,16 +86,46 @@ public:
   bool is_struct() override { return true; }
 
   std::any get() override { throw std::runtime_error("Not implemented"); }
+  std::any get(std::string key) { return fields[key]->get(); }
 
-  std::any get(std::string key) override { return fields[key]->get(); }
-
-  template <typename... Ts> std::any get(std::string key, Ts... args) {
+  template <typename T, typename... Ts> T get(std::string key, Ts &&...args) {
     if (key == "_") {
-      assert(this->parent->is_struct());
-      return this->parent->get(args...);
+      if constexpr (sizeof...(args) > 0) {
+        assert(this->parent != nullptr);
+        assert(this->parent->is_struct());
+        return ((Struct *)this->parent)->get<T>(args...);
+      } else {
+        throw std::runtime_error("Not implemented");
+      }
     } else {
-      assert(this->parent->is_struct());
-      return fields[key]->get(args...);
+      if constexpr (sizeof...(args) == 0) {
+        return std::any_cast<T>(fields[key]->get());
+      } else {
+        assert(fields[key]->is_struct());
+        return ((Struct *)fields[key])->get<T>(args...);
+      }
+    }
+  }
+
+  Base *get_field(std::string key) { return fields[key]; }
+
+  template <typename T, typename... Ts>
+  T *get_field(std::string key, Ts &&...args) {
+    if (key == "_") {
+      if constexpr (sizeof...(args) > 0) {
+        assert(this->parent != nullptr);
+        assert(this->parent->is_struct());
+        return ((Struct *)this->parent)->get_field<T>(args...);
+      } else {
+        throw std::runtime_error("Not implemented");
+      }
+    } else {
+      if constexpr (sizeof...(args) == 0) {
+        return (T *)fields[key];
+      } else {
+        assert(fields[key]->is_struct());
+        return ((Struct *)fields[key])->get_field<T>(args...);
+      }
     }
   }
 };
--- tests/tests.cpp
+++ tests/tests.cpp
@@ -30,6 +30,20 @@ TEST_CASE("Int building test") {
   REQUIRE(ss.get() == orig.get());
 }
 
+TEST_CASE("Int8ul parsing test") {
+  etc::Int8ul field;
+  std::stringstream ss, orig;
+  int8_t i = 0x12;
+  ss.write(reinterpret_cast<const char *>(&i), sizeof(i));
+  orig.write(reinterpret_cast<const char *>(&i), sizeof(i));
+  REQUIRE(std::any_cast<int8_t>(field.parse(ss)) == i);
+  REQUIRE(field.value == i);
+
+  ss.seekg(0);
+  orig.seekg(0);
+  REQUIRE(ss.get() == orig.get());
+}
+
 TEST_CASE("Structs parsing") {
   etc::Struct s(etc::Field("a", new etc::Int32ul()),
                 etc::Field("b", new etc::Int32ul()));
@@ -44,8 +58,8 @@ TEST_CASE("Structs parsing") {
   REQUIRE(std::any_cast<int32_t>(obj["a"]) == a);
   REQUIRE(std::any_cast<int32_t>(obj["b"]) == b);
 
-  auto a_out = std::any_cast<int32_t>(s.get("a"));
-  auto b_out = std::any_cast<int32_t>(s.get("b"));
+  auto a_out = s.get<int32_t>("a");
+  auto b_out = s.get<int32_t>("b");
 
   REQUIRE(a_out == a);
   REQUIRE(b_out == b);
@@ -68,9 +82,18 @@ TEST_CASE("nested Structs") {
 
   std::map<std::string, std::any> obj =
       std::any_cast<std::map<std::string, std::any>>(s.parse(data));
+
+  REQUIRE(s.get<int32_t>("a") == a);
+  REQUIRE(s.get<int32_t>("b") == b);
+  REQUIRE(s.get<int32_t>("c", "d") == d);
+  REQUIRE(s.get<int32_t>("c", "e") == e);
+  REQUIRE(s.get<int32_t>("c", "_", "a") == a);
+  REQUIRE(s.get<int32_t>("c", "_", "b") == b);
+
+  s.get_field<etc::Int32ul>("c", "_", "a")->value = 12;
+  REQUIRE(s.get<int32_t>("c", "_", "a") == 12);
 }
 
-/*
 TEST_CASE("Struct building") {
   etc::Struct s(etc::Field("a", new etc::Int32ul()),
                 etc::Field("b", new etc::Int32ul()));
@@ -79,8 +102,8 @@ TEST_CASE("Struct building") {
   int32_t b = 0x87654321;
   data.write(reinterpret_cast<const char *>(&a), sizeof(a));
   data.write(reinterpret_cast<const char *>(&b), sizeof(b));
-  s.get("a") = a;
-  s.get("b") = b;
+  s.get_field<etc::Int32ul>("a")->value = a;
+  s.get_field<etc::Int32ul>("b")->value = b;
   std::stringstream ss;
   s.build(ss);
   ss.seekg(0);
@@ -90,4 +113,5 @@ TEST_CASE("Struct building") {
   REQUIRE(ss.get() == data.get());
   REQUIRE(ss.get() == data.get());
 }
-*/
+
+TEST_CASE("Array") { etc::Array<etc::Int32ul>(8); }
