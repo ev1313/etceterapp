05642d271977ae698babee03f34a3a4ffc612343
Tim Blume <tbl@sevenstax-intern.de>
2024-01-22 19:31:35 +0100

    pointer test

--- tests/pointer_tests.cpp
+++ tests/pointer_tests.cpp
@@ -26,3 +26,40 @@ TEST_CASE("Pointer parse test") {
   REQUIRE(s->get<uint32_t>("b") == b);
   REQUIRE(orig.tellg() == 4);
 }
+
+TEST_CASE("Pointer build test") {
+  auto s = Struct::create(Field("a", Int32ul::create()),
+                          Field("b", Pointer::create(
+                                         [](std::weak_ptr<Base> c) {
+                                           return lock(c)->get<uint32_t>("a");
+                                         },
+                                         Int32ul::create())),
+                          Field("c", Int32ul::create()),
+                          Field("d", Int32ul::create()),
+                          Field("e", Int32ul::create()));
+  uint32_t a = 12;
+  uint32_t b = 456;
+  std::stringstream orig;
+  orig.write(reinterpret_cast<const char *>(&a), sizeof(a));
+  REQUIRE(orig.tellp() == 4);
+  orig.write(reinterpret_cast<const char *>(&a), sizeof(a));
+  REQUIRE(orig.tellp() == 8);
+  orig.write(reinterpret_cast<const char *>(&a), sizeof(a));
+  REQUIRE(orig.tellp() == 12);
+  orig.write(reinterpret_cast<const char *>(&a), sizeof(a));
+  REQUIRE(orig.tellp() == 16);
+  orig.write(reinterpret_cast<const char *>(&b), sizeof(b));
+  REQUIRE(orig.tellp() == 20);
+
+  std::string str;
+  str.resize(s->get_size({}), '\0');
+  std::stringstream ss(str);
+  s->parse(orig);
+  s->build(ss);
+  ss.seekg(0, std::ios::beg);
+  orig.seekg(0, std::ios::beg);
+  for (int i = 0; i < 5; i++) {
+    REQUIRE(ss.get() == orig.get());
+  }
+  REQUIRE(ss.str() == orig.str());
+}
